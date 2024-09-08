trezor/trezor-firmware

feat(core/embed): introduce systick and systimer drivers
[no changelog]

core/embed/trezorhal/stm32f4/systick.c
@@ -1,5 +1,3 @@
-// clang-format off
-
 /*
  * This file is part of the Trezor project, https://trezor.io/
  *
@@ -19,52 +17,179 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of the MicroPython project, http://micropython.org/
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2013, 2014 Damien P. George
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+#include <stdbool.h>
+#include <stddef.h>
 
 #include "irq.h"
-#include "systick.h"
-
-#ifdef RDI
-  #include "random_delays.h"
-#endif
-
+#include "platform.h"
 #include "systemview.h"
 
-extern __IO uint32_t uwTick;
+#include "systick.h"
+#include "systick_internal.h"
+#include "systimer.h"
+
+// SysTick driver state
+typedef struct {
+  // Set if the driver is initialized
+  bool initialized;
+  // Number of hw cycles per millisecond (tick period)
+  uint32_t cycles_per_ms;
+  // Number of hw cycles per microsecond
+  uint32_t cycles_per_us;
+  // Current tick value in hardware cycles
+  volatile uint64_t cycles;
+  // Number of ticks (ms) since the system start
+  volatile uint32_t ticks;
+} systick_driver_t;
+
+static systick_driver_t g_systick_driver = {
+    .initialized = false,
+};
+
+void systick_init(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (drv->initialized) {
+    return;
+  }
+
+  drv->cycles = 0;
+  drv->ticks = 0;
+
+  // Set 1ms tick period
+  drv->cycles_per_ms = HAL_RCC_GetSysClockFreq() / 1000;
+  drv->cycles_per_us = drv->cycles_per_ms / 1000;
+
+  // We need to ensure that SysTick has the expected priority.
+  // The SysTick priority is configured in the boardloader,
+  // and some early versions didn't set this properly.
+  NVIC_SetPriority(SysTick_IRQn, IRQ_PRI_HIGHEST);
+
+  // Initialize and ebavle SysTick timer
+  SysTick_Config(drv->cycles_per_ms);
+
+  drv->initialized = true;
+}
+
+void systick_deinit(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (!drv->initialized) {
+    return;
+  }
+
+  NVIC_DisableIRQ(SysTick_IRQn);
+  SysTick->CTRL = 0;
+  NVIC_ClearPendingIRQ(SysTick_IRQn);
+
+  drv->initialized = false;
+}
+
+void systick_update_freq(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (!drv->initialized) {
+    return;
+  }
+
+  uint32_t clock_freq = HAL_RCC_GetSysClockFreq();
+  drv->cycles_per_ms = clock_freq / 1000;
+  drv->cycles_per_us = drv->cycles_per_ms / 1000;
+  SysTick_Config(drv->cycles_per_ms);
+}
+
+uint64_t systick_cycles(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  uint32_t irq_state = disable_irq();
+
+  // Current value of the SysTick counter
+  uint32_t val = SysTick->VAL;
+
+  // Check if the SysTick has already counted down to 0 or wrapped around
+  // Reading CTRL register automatically clears COUNTFLAG
+  if ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != 0) {
+    // Re-read the current value of the SysTick counter
+    val = SysTick->VAL;
+    // Update the hardware cycles counter
+    // Since we have cleared COUNTFLAG, SysTick_Handler will not increment
+    drv->cycles += drv->cycles_per_ms;
+    // Increment regular ticks counter
+    drv->ticks++;
+  }
+
+  uint64_t cycles = drv->cycles + ((val > 0) ? (drv->cycles_per_ms - val) : 0);
+
+  enable_irq(irq_state);
+
+  return cycles;
+}
+
+uint64_t systick_us_to_cycles(uint64_t us) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  return us * drv->cycles_per_us;
+}
+
+uint32_t systick_ms(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  return drv->ticks;
+}
+
+uint64_t systick_us(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  return systick_cycles() / drv->cycles_per_us;
+}
 
 void SysTick_Handler(void) {
   SEGGER_SYSVIEW_RecordEnterISR();
-  // this is a millisecond tick counter that wraps after approximately
-  // 49.71 days = (0xffffffff / (24 * 60 * 60 * 1000))
-  uint32_t uw_tick = uwTick + 1;
-  uwTick = uw_tick;
-#ifdef RDI
-    rdi_handler(uw_tick);
-#endif
+
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (drv->initialized) {
+    // Increment `cycles` counter if COUNTFLAG is set.
+    // If COUNTFLAG is not set, `cycles` were already incremented.
+    // in `systick_cycles()` that also cleared the COUNFLAG.
+    if ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != 0) {
+      // Increment cycles counter by SysTick period
+      drv->cycles += drv->cycles_per_ms;
+      // Increment regular ticks counter
+      drv->ticks++;
+    }
+
+    // Invoke callbacks of expired timers
+    systimer_dispatch_expired_timers(drv->cycles);
+  }
 
   SEGGER_SYSVIEW_RecordExitISR();
 }
+
+void systick_delay_us(uint64_t us) {
+  uint64_t delay_cycles = systick_us_to_cycles(us);
+  int64_t cycles_per_ms = systick_us_to_cycles(1000);
+
+  uint64_t end = systick_cycles() + delay_cycles;
+  bool irq_enabled = IS_IRQ_ENABLED(query_irq());
+  int64_t diff;
+
+  while ((diff = end - systick_cycles()) > 0) {
+    if (irq_enabled && (diff > cycles_per_ms)) {
+      // Enter sleep mode and wait for (at least)
+      // the SysTick interrupt.
+      __WFI();
+    }
+  }
+}
+
+void systick_delay_ms(uint32_t ms) { systick_delay_us((uint64_t)ms * 1000); }
+
+// We provide our own version of HAL_Delay that calls __WFI while waiting,
+// and works when interrupts are disabled.  This function is intended to be
+// used only by the ST HAL functions.
+void HAL_Delay(uint32_t ms) { systick_delay_ms(ms); }
+
+// We provide our own version of HAL_GetTick that replaces the default
+// ST HAL function reading uwTick global variable.
+uint32_t HAL_GetTick(void) { return systick_ms(); }

Redblaze420/main

core/embed/trezorhal/stm32f4/systick.c
@@ -1,5 +1,3 @@
-// clang-format off
-
 /*
  * This file is part of the Trezor project, https://trezor.io/
  *
@@ -19,52 +17,181 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of the MicroPython project, http://micropython.org/
- *
- * The MIT License (MIT)
- *
- * Copyright (c) 2013, 2014 Damien P. George
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+#include <stdbool.h>
+#include <stddef.h>
 
 #include "irq.h"
-#include "systick.h"
-
-#ifdef RDI
-  #include "random_delays.h"
-#endif
-
+#include "platform.h"
 #include "systemview.h"
 
-extern __IO uint32_t uwTick;
+#include "systick.h"
+#include "systick_internal.h"
+#include "systimer.h"
+
+// SysTick driver state
+typedef struct {
+  // Set if the driver is initialized
+  bool initialized;
+  // Number of hw cycles per millisecond (tick period)
+  uint32_t cycles_per_ms;
+  // Number of hw cycles per microsecond
+  uint32_t cycles_per_us;
+  // Current tick value in hardware cycles
+  volatile uint64_t cycles;
+  // Number of ticks (ms) since the system start
+  volatile uint32_t ticks;
+} systick_driver_t;
+
+static systick_driver_t g_systick_driver = {
+    .initialized = false,
+};
+
+void systick_init(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (drv->initialized) {
+    return;
+  }
+
+  drv->cycles = 0;
+  drv->ticks = 0;
+
+  // Set 1ms tick period
+  drv->cycles_per_ms = HAL_RCC_GetSysClockFreq() / 1000;
+  drv->cycles_per_us = drv->cycles_per_ms / 1000;
+
+  // Initialize and enable SysTick timer
+  SysTick_Config(drv->cycles_per_ms);
+
+  // We need to ensure that SysTick has the expected priority.
+  // The SysTick priority is configured in the boardloader,
+  // and some early versions didn't set this properly.
+  NVIC_SetPriority(SysTick_IRQn, IRQ_PRI_NORMAL);
+
+  drv->initialized = true;
+}
+
+void systick_deinit(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (!drv->initialized) {
+    return;
+  }
+
+  NVIC_DisableIRQ(SysTick_IRQn);
+  SysTick->CTRL = 0;
+  NVIC_ClearPendingIRQ(SysTick_IRQn);
+
+  drv->initialized = false;
+}
+
+void systick_update_freq(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (!drv->initialized) {
+    return;
+  }
+
+  uint32_t clock_freq = HAL_RCC_GetSysClockFreq();
+  drv->cycles_per_ms = clock_freq / 1000;
+  drv->cycles_per_us = drv->cycles_per_ms / 1000;
+  SysTick_Config(drv->cycles_per_ms);
+}
+
+uint64_t systick_cycles(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  uint32_t irq_state = disable_irq();
+
+  // Current value of the SysTick counter
+  uint32_t val = SysTick->VAL;
+
+  // Check if the SysTick has already counted down to 0 or wrapped around
+  // Reading CTRL register automatically clears COUNTFLAG
+  if ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != 0) {
+    // Re-read the current value of the SysTick counter
+    val = SysTick->VAL;
+    // Update the hardware cycles counter
+    // Since we have cleared COUNTFLAG, SysTick_Handler will not increment
+    drv->cycles += drv->cycles_per_ms;
+    // Increment regular ticks counter
+    drv->ticks++;
+  }
+
+  uint64_t cycles = drv->cycles + ((val > 0) ? (drv->cycles_per_ms - val) : 0);
+
+  enable_irq(irq_state);
+
+  return cycles;
+}
+
+uint64_t systick_us_to_cycles(uint64_t us) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  return us * drv->cycles_per_us;
+}
+
+uint32_t systick_ms(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  return drv->ticks;
+}
+
+uint64_t systick_us(void) {
+  systick_driver_t* drv = &g_systick_driver;
+
+  return systick_cycles() / drv->cycles_per_us;
+}
 
 void SysTick_Handler(void) {
   SEGGER_SYSVIEW_RecordEnterISR();
-  // this is a millisecond tick counter that wraps after approximately
-  // 49.71 days = (0xffffffff / (24 * 60 * 60 * 1000))
-  uint32_t uw_tick = uwTick + 1;
-  uwTick = uw_tick;
-#ifdef RDI
-    rdi_handler(uw_tick);
-#endif
+
+  systick_driver_t* drv = &g_systick_driver;
+
+  if (drv->initialized) {
+    // Increment `cycles` counter if COUNTFLAG is set.
+    // If COUNTFLAG is not set, `cycles` were already incremented
+    // in `systick_cycles()` that also cleared the COUNTFLAG.
+    if ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != 0) {
+      // Clear COUNTFLAG by reading VAL
+      (void)SysTick->VAL;
+      // Increment cycles counter by SysTick period
+      drv->cycles += drv->cycles_per_ms;
+      // Increment regular ticks counter
+      drv->ticks++;
+    }
+
+    // Invoke callbacks of expired timers
+    systimer_dispatch_expired_timers(drv->cycles);
+  }
 
   SEGGER_SYSVIEW_RecordExitISR();
 }
+
+void systick_delay_us(uint64_t us) {
+  uint64_t delay_cycles = systick_us_to_cycles(us);
+  int64_t cycles_per_ms = systick_us_to_cycles(1000);
+
+  uint64_t end = systick_cycles() + delay_cycles;
+  bool irq_enabled = IS_IRQ_ENABLED(query_irq());
+  int64_t diff;
+
+  while ((diff = end - systick_cycles()) > 0) {
+    if (irq_enabled && (diff > cycles_per_ms)) {
+      // Enter sleep mode and wait for (at least)
+      // the SysTick interrupt.
+      __WFI();
+    }
+  }
+}
+
+void systick_delay_ms(uint32_t ms) { systick_delay_us((uint64_t)ms * 1000); }
+
+// We provide our own version of HAL_Delay that calls __WFI while waiting,
+// and works when interrupts are disabled.  This function is intended to be
+// used only by the ST HAL functions.
+void HAL_Delay(uint32_t ms) { systick_delay_ms(ms); }
+
+// We provide our own version of HAL_GetTick that replaces the default
+// ST HAL function reading uwTick global variable.
+uint32_t HAL_GetTick(void) { return systick_ms(); }